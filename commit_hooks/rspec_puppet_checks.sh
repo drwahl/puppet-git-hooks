4261fb182b4786b428dd9a37c9cdec2019c727af
David Wahlstrom <david.wahlstrom@isilon.com>
2014-03-16 09:52:24 -0700

    only run rspec once per module

--- commit_hooks/rspec_puppet_checks.sh
+++ commit_hooks/rspec_puppet_checks.sh
@@ -6,9 +6,21 @@ error_msg=$(mktemp /tmp/error_msg_rspec-puppet.XXXXX)
 
 # Run rspec-puppet tests
 if [ `which rspec` ]; then
-    for puppetmodule in `git diff --cached --name-only --diff-filter=ACM | grep '\.*.pp$\|\.*.rb$'`; do
-        module_dir=$(dirname $puppetmodule | cut -d"/" -f1,2)
+    tmpchangedmodules=''
+    #get a list of files changed under the modules directory so we can
+    #sort/uniq them later
+    for changedfile in `git diff --raw --cached --name-only --diff-filter=ACM | grep '^modules' | grep '\.*.pp$\|\.*.rb$'`; do
+        changeddir=$(dirname $changedfile | cut -d"/" -f1,2)
+        tmpchangedmodules="$tmpchangedmodules\n$changeddir"
+    done
+
+    #sort/uniq so we only run rspec tests once
+    changedmodules=$(echo $tmpchangedmodules | sort -u)
+
+    #now that we have the list of modules that changed, run rspec for each module
+    for module_dir in $changedmodules; do
         cd $module_dir
+        #this will run rspec for every test in the module
         rspec > $error_msg
         RC=$?
         cd - > /dev/null
