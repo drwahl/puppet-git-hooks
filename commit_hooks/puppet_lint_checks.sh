afea2419f185de554de853379a5b7cc3e6988532
Dr Wahl <david.wahlstrom@gmail.com>
2014-03-24 06:49:55 -0700

    Check for double quotes

--- commit_hooks/puppet_lint_checks.sh
+++ commit_hooks/puppet_lint_checks.sh
@@ -6,7 +6,7 @@ syntax_errors=0
 # De-lint puppet manifests
 if [ `which puppet-lint` ]; then
     for puppetmodule in `git diff --cached --name-only --diff-filter=ACM | grep \.*.pp$`; do
-        puppet-lint --fail-on-warnings --with-filename --no-80chars-check --no-double_quoted_strings-check $git_root/$puppetmodule
+        puppet-lint --fail-on-warnings --with-filename --no-80chars-check $git_root/$puppetmodule
         RC=$?
         if [ $RC -ne 0 ]; then
             syntax_errors=$(expr $syntax_errors + 1)
