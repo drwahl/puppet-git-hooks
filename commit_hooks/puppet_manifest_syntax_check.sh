6cf9441fbf47d0cbb3f4eb37c6d2f0e4aac11181
Henry Goodman <github@henrygoodman.com>
2014-10-21 19:17:07 -0700

    Added tput-based coloring to pre-receive, puppet_lint_checks.sh and puppet_manifest_syntaxt_check.sh. Fixed the STDERR output redirection for puppet_lint_checks.sh and puppet_manifest_syntaxt_check.sh.

--- commit_hooks/puppet_manifest_syntax_check.sh
+++ commit_hooks/puppet_manifest_syntax_check.sh
@@ -3,29 +3,31 @@
 # This script expects $1 to be passed and for $1 to be the filesystem location
 # to a puppet manifest file for which it will run syntax checks against.
 
+manifest_path="$1"
+module_dir="$2"
+
 syntax_errors=0
 error_msg=$(mktemp /tmp/error_msg_puppet-syntax.XXXXX)
 
-if [ $2 ]; then
-    module_path=$(echo $1 | sed -e 's|'$2'||')
+if [ $module_dir ]; then
+    manifest_name=$(echo $manifest_path | sed -e 's|'$module_dir'||')
 else
-    module_path=$1
+    manifest_name="$manifest_path"
 fi
 
 # Get list of new/modified manifest and template files to check (in git index)
 # Check puppet manifest syntax
-echo -e "\x1B[0;36mChecking puppet manifest syntax for $module_path...\x1B[0m"
-puppet parser validate --color=false $1 2>&1 > $error_msg
+echo -e "$(tput setaf 6)Checking puppet manifest syntax for $manifest_name...$(tput sgr0)"
+puppet parser validate --color=false $1 > $error_msg 2>&1
 if [ $? -ne 0 ]; then
-    echo -en "\x1B[0;31m"
     syntax_errors=`expr $syntax_errors + 1`
-    cat $error_msg
-    echo -e "Error: puppet syntax error in $module_path (see above)\x1B[0m"
+    cat $error_msg | sed -e 's|'$module_dir'||' -e "s/^/$(tput setaf 1)/" -e "s/$/$(tput sgr0)/"
+    echo -e "$(tput setaf 1)Error: puppet syntax error in $manifest_name (see above)$(tput sgr0)"
 fi
 rm -f $error_msg
 
 if [ "$syntax_errors" -ne 0 ]; then
-    echo -e "\x1B[0;31mError: $syntax_errors syntax error(s) found in puppet manifests. Commit will be aborted.\x1B[0m"
+    echo -e "$(tput setaf 1)Error: $syntax_errors syntax error(s) found in puppet manifests. Commit will be aborted.$(tput sgr0)"
     exit 1
 fi
 
