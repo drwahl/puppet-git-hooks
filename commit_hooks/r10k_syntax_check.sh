9d534e7f3804622f4438c5e196dbf274cf91fcf4
Jan Collijs <jan.collijs@inuits.eu>
2015-03-25 14:09:48 +0100

    Feature: configured r10k check if changedfile is Puppetfile

--- commit_hooks/r10k_syntax_check.sh
+++ commit_hooks/r10k_syntax_check.sh
@@ -2,14 +2,10 @@
 #
 # This script assumes you have installed r10k and will perform a syntax check on the Puppetfile if existing
 
-if [ -f Puppetfile ];
-then
-	echo "Performing a syntax check on the r10k Puppetfile:"
-	r10k puppetfile check
-fi
+echo "Performing a syntax check on the r10k Puppetfile:"
+r10k puppetfile check
 
 if [[ $? -ne 0 ]]
 then
-	        exit 1
-	fi
-
+	exit 1
+fi
