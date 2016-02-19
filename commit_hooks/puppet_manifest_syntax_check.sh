f46c10bf2a30a7f78aecb1f326cc0c0fe920bef8
NateMcCurdy <nate.mccurdy@puppetlabs.com>
2016-02-19 13:49:33 -0800

    Fix BASHisms in puppet_manifest_syntax_check

--- commit_hooks/puppet_manifest_syntax_check.sh
+++ commit_hooks/puppet_manifest_syntax_check.sh
@@ -10,8 +10,8 @@ USE_PUPPET_FUTURE_PARSER="$3"
 syntax_errors=0
 error_msg=$(mktemp /tmp/error_msg_puppet-syntax.XXXXX)
 
-if [ $module_dir ]; then
-    manifest_name=$(echo $manifest_path | sed -e 's|'$module_dir'||')
+if [[ $module_dir ]]; then
+    manifest_name="${manifest_path##*$module_dir}"
     error_msg_filter="sed -e s|$module_dir||"
 else
     manifest_name="$manifest_path"
@@ -21,20 +21,20 @@ fi
 # Get list of new/modified manifest and template files to check (in git index)
 # Check puppet manifest syntax
 echo -e "$(tput setaf 6)Checking puppet manifest syntax for $manifest_name...$(tput sgr0)"
-if [ "$USE_PUPPET_FUTURE_PARSER" != "enabled" ]; then
-    puppet parser validate --color=false $1 > $error_msg 2>&1
+if [[ $USE_PUPPET_FUTURE_PARSER != "enabled" ]]; then
+    puppet parser validate --color=false "$1" > "$error_msg" 2>&1
 else
-    puppet parser validate --parser future --color=false $1 > $error_msg 2>&1
+    puppet parser validate --parser future --color=false "$1" > "$error_msg" 2>&1
 fi
 
-if [ $? -ne 0 ]; then
-    syntax_errors=`expr $syntax_errors + 1`
-    cat $error_msg | $error_msg_filter -e "s/^/$(tput setaf 1)/" -e "s/$/$(tput sgr0)/"
+if [[ $? -ne 0 ]]; then
+  syntax_errors=$((syntax_errors + 1))
+    $error_msg_filter -e "s/^/$(tput setaf 1)/" -e "s/$/$(tput sgr0)/" < "$error_msg"
     echo -e "$(tput setaf 1)Error: puppet syntax error in $manifest_name (see above)$(tput sgr0)"
 fi
-rm -f $error_msg
+rm -f "$error_msg"
 
-if [ "$syntax_errors" -ne 0 ]; then
+if [[ $syntax_errors -ne 0 ]]; then
     echo -e "$(tput setaf 1)Error: $syntax_errors syntax error(s) found in puppet manifests. Commit will be aborted.$(tput sgr0)"
     exit 1
 fi
