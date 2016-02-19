dcfd201d572fc85a025e00f3af6ac7bc9c1cf533
NateMcCurdy <nate.mccurdy@puppetlabs.com>
2016-02-19 13:55:59 -0800

    Fix BASHisms in erb_template_syntax_check

--- commit_hooks/erb_template_syntax_check.sh
+++ commit_hooks/erb_template_syntax_check.sh
@@ -6,23 +6,23 @@
 # particularly useful for server-side hooks when tempdirs are created.
 
 syntax_errors=0
-error_msg=$(mktemp /tmp/error_msg_erb-syntax.XXXXX)
+error_msg="$(mktemp /tmp/error_msg_erb-syntax.XXXXX)"
 
-if [ $2 ]; then
-    module_path=$(echo $1 | sed -e 's|'$2'||')
+if [[ $2 ]]; then
+    module_path="${1##*$2}"
 else
     module_path=$1
 fi
 
 # Check ERB template syntax
 echo -e "$(tput setaf 6)Checking erb template syntax for $module_path...$(tput sgr0)"
-cat $1 | erb -P -x -T - | ruby -c > $error_msg 2>&1
+erb -P -x -T - "$1" | ruby -c 2> "$error_msg" 1>&2
 if [ $? -ne 0 ]; then
-    cat $error_msg | sed -e "s/^/$(tput setaf 1)/" -e "s/$/$(tput sgr0)/"
-    syntax_errors=`expr $syntax_errors + 1`
+    sed -e "s/^/$(tput setaf 1)/" -e "s/$/$(tput sgr0)/" "$error_msg"
+    syntax_errors=$((syntax_errors + 1))
     echo -e "$(tput setaf 1)Error: erb syntax error in $module_path (see above)$(tput sgr0)"
 fi
-rm $error_msg
+rm -f "$error_msg"
 
 if [ "$syntax_errors" -ne 0 ]; then
     echo -e "$(tput setaf 1)Error: $syntax_errors syntax errors found in templates. Commit will be aborted.$(tput sgr0)"
