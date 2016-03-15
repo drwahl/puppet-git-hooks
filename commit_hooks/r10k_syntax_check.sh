f26a91b95bcc70ff70b1dc7deb8ef4b42ecaa665
Steven Bambling <smbambling@gmail.com>
2016-03-15 08:55:25 -0400

    adding pre-receive r10k check and moving away from which to build in bash hash

--- commit_hooks/r10k_syntax_check.sh
+++ commit_hooks/r10k_syntax_check.sh
@@ -3,7 +3,7 @@
 # This script assumes you have installed r10k and will perform a syntax check on the Puppetfile if existing
 
 echo "Performing a syntax check on the r10k Puppetfile:"
-r10k puppetfile check
+PUPPETFILE="$1" r10k puppetfile check
 
 if [[ $? -ne 0 ]]
 then
