6c87d1a26e38988944297478679a139fa06e8e25
Yehuda Katz <yehuda@ymkatz.net>
2020-10-29 22:17:46 -0400

    Update r10k command line options

--- commit_hooks/r10k_syntax_check.sh
+++ commit_hooks/r10k_syntax_check.sh
@@ -3,7 +3,7 @@
 # This script assumes you have installed r10k and will perform a syntax check on the Puppetfile if existing
 
 echo "Performing a syntax check on the r10k Puppetfile:"
-PUPPETFILE="$1" r10k puppetfile check
+r10k puppetfile check --puppetfile "$1"
 
 if [[ $? -ne 0 ]]
 then
